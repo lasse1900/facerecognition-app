be338c427b82c41bf86c02198a5feff7fdceead2
Lauri Kyttälä <lauri.kyttala@gmail.com>
2021-09-16 17:12:48 +0300

    initial commit - basic facerecognition-app layout

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import './App.css';
+import React from 'react';
+import Navigation from './Components/Navigation/Navigation';
+import Logo from './Components/Logo/Logo';
+import Rank from './Components/Rank/Rank';
+import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
+import Particles from "react-tsparticles";
+
+const particleOptions = {
+    particles: {
+      color: {
+        value: "#ffffff",
+      },
+      links: {
+        color: "#ffffff",
+        distance: 150,
+        enable: true,
+        opacity: 0.5,
+        width: 1,
+      },
+      collisions: {
+        enable: true,
+      },
+      move: {
+        direction: "none",
+        enable: true,
+        outMode: "bounce",
+        random: false,
+        speed: 6,
+        straight: false,
+      },
+      number: {
+        density: {
+          enable: true,
+          value_area: 800,
+        },
+        value: 80,
+      },
+      opacity: {
+        value: 0.5,
+      },
+      shape: {
+        type: "circle",
+      },
+      size: {
+        random: true,
+        value: 5,
+      },
+  },
+  detectRetina: true,
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Particles className='particles' params={particleOptions} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm />
+        {/*<FaceRecognition /> */}
+      </div>
+    )
+  }
+}
+
+export default App;
