1ccb313a75a891657703d656633c307db24446c2
Lauri Kyttälä <lauri.kyttala@gmail.com>
2021-09-17 11:02:06 +0300

    first step read jpg

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import Logo from './Components/Logo/Logo';
 import Rank from './Components/Rank/Rank';
 import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
 import Particles from "react-tsparticles";
+import Clarifai from 'clarifai';
+
+const app = new Clarifai.App({
+  apiKey: "780aa9f8b3fa4098aa967044b7caffba",
+});
 
 const particleOptions = {
   particles: {
@@ -26,7 +31,7 @@ const particleOptions = {
       enable: true,
       outMode: "bounce",
       random: false,
-      speed: 6,
+      speed: 2,
       straight: false,
     },
     number: {
@@ -62,24 +67,36 @@ class App extends React.Component {
     console.log(event.target.value)
   }
 
-  onButtonSubmit = () => {
-    console.log('click')
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <Particles className='particles' params={particleOptions} />
-        <Navigation />
-        <Logo />
-        <Rank />
-        <ImageLinkForm
-          onInputChange={this.onInputChange}
-          onButtonSubmit={this.onButtonSubmit} />
-        {/*<FaceRecognition /> */}
-      </div>
+onButtonSubmit = () => {
+  console.log("click");
+  app.models
+    .predict(
+      Clarifai.FACE_DETECT_MODEL,
+      // THE JPG
+      "https://i.insider.com/5d321d4ea209d3146d650b4a?width=1100&format=jpeg&auto=webp"
     )
-  }
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+render() {
+  return (
+    <div className="App">
+      <Particles className='particles' params={particleOptions} />
+      <Navigation />
+      <Logo />
+      <Rank />
+      <ImageLinkForm
+        onInputChange={this.onInputChange}
+        onButtonSubmit={this.onButtonSubmit} />
+      {/*<FaceRecognition /> */}
+    </div>
+  )
+}
 }
 
 export default App;
