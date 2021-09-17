ec7b8c657ae9181f02761f0333bb7548d435ee8a
Lauri Kyttälä <lauri.kyttala@gmail.com>
2021-09-17 13:14:04 +0300

    third step done recognition working

--- src/App.js
+++ src/App.js
@@ -61,12 +61,31 @@ class App extends React.Component {
     super();
     this.state = {
       input: '',
-      imageUrl: ''
+      imageUrl: '',
+      box: {}
     }
   }
 
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputImage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    console.log(box)
+    this.setState({ box: box })
+  }
+
   onInputChange = (event) => {
-    this.setState({input: event.target.value})
+    this.setState({ input: event.target.value })
   }
 
   onButtonSubmit = () => {
@@ -78,12 +97,8 @@ class App extends React.Component {
         // THE JPG
         this.state.input
       )
-      .then((response) => {
-        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .catch((err) => console.log(err));
   };
 
   render() {
@@ -96,7 +111,7 @@ class App extends React.Component {
         <ImageLinkForm
           onInputChange={this.onInputChange}
           onButtonSubmit={this.onButtonSubmit} />
-        <FaceRecognition imageUrl={this.state.imageUrl} />
+        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
       </div>
     )
   }
