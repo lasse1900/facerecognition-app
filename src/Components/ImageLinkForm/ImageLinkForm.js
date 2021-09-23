7e852a1d46109d0ca132e8c2b422df61712dcf7c
Lauri Kyttälä <lauri.kyttala@gmail.com>
2021-09-23 15:23:37 +0300

    edited user info texts

--- src/Components/ImageLinkForm/ImageLinkForm.js
+++ src/Components/ImageLinkForm/ImageLinkForm.js
@@ -5,13 +5,14 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
   return (
     <div>
       <p className='f3'>
-        {'This Magic Brain will detect fases in your pictures'}
+        {'This App detects fases in your pictures, try e.g. this: '}
+        {'https://www.thestatesman.com/wp-content/uploads/2017/08/1493458748-beauty-face-517.jpg '}
       </p>
       <div className='center'>
         <div className='form center pa4 br3 shadow-5'>
-          <input className='f4 pa2 w-70 center' type='text' onChange={onInputChange}/>
-          <button 
-          className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'
+          <input className='f4 pa2 w-70 center' type='text' onChange={onInputChange} />
+          <button
+            className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'
             onClick={onButtonSubmit}
           >Detect</button>
         </div>
